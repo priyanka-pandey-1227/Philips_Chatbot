2095d2ce5225db63c72c132b00326883f9dd3e2f
priyanka.pandey <priyankapandey2712@gmail.com>
2019-07-25 12:29:49 +0530

    Commit on 25 july

--- chatbot_monitors.sql
+++ chatbot_monitors.sql
@@ -35,7 +35,7 @@ CREATE TABLE `monitors` (
   `temperature` varchar(45) DEFAULT 'NO',
   `link` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- chatbot_users.sql
+++ chatbot_users.sql
@@ -29,7 +29,7 @@ CREATE TABLE `users` (
   `hospital` varchar(45) DEFAULT NULL,
   `userscol` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
